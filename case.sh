4e8f1a4909692a13e7fa12830195b33d401e5ddf
hemanaik <hemanaik@gmail.com>
2022-11-08 11:33:48 +0000

    added one more case in case.sh

--- case.sh
+++ case.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo "1:show date 2:list files 3:show current path"
+echo "1:show date 2:list files 3:show current path 4:check RAM"
 read choice
 case $choice in
         1)date;;
         2)ls -lrt;;
         3)pwd;;
+	4)free -h;;
         *)echo "invalid"
 esac
-echo "end of pgm"
+echo "end of pgm......."
